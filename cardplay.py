93360ca6790efd47d32951b8d9e5171a42a4c51d
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-05 20:32:21 -0800

    added deck class for creating Deck of cards

--- cardplay.py
+++ cardplay.py
@@ -17,16 +17,30 @@ class Card():
 
 
     def __eq__(self, other):
-        
+
         return self.suit == other.suit and self.rank == other.rank
 
     def __lt__(self, other):
-        
+
         t1 = self.suit, self.rank
         t2 = other.suit, other.rank
         return t1 < t2
 
+class Deck:
+    """ This represents a deck of Cards """
 
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
 
 
 
