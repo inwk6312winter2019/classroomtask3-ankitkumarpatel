0c64f4a933ddf3a7c9885a9e9fd10bb257d588ea
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-05 21:16:18 -0800

    Fixed few errors

--- cardplay.py
+++ cardplay.py
@@ -1,66 +1,66 @@
 # my code for card play game
 
-
+import random
+import math
+import string
 
 class Card():
    """ This creates a card object"""
 
-    suit_names = ['Clubs','Diamonds','Hearts','Spades']
-    rank_names = [None,'Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+   suit_names = ['Clubs','Diamonds','Hearts','Spades']
+   rank_names = [None,'Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 
-    def __init__(self,suit=1,rank=12):
-        self.suit=suit
-        self.rank=rank
+   def __init__(self,suit=1,rank=12):
+       self.suit=suit
+       self.rank=rank
+   def __str__(self):
+       return '%s of %s' % (Card.rank_names[self.rank],Card.suit_names[self.suit])
 
-    def __str__(self):
-        return '%s of %s' % (Card.rank_names[self.rank],Card.suit_names[self.suit])
 
+   def __eq__(self, other):
 
-    def __eq__(self, other):
+       return self.suit == other.suit and self.rank == other.rank
 
-        return self.suit == other.suit and self.rank == other.rank
+   def __lt__(self, other):
 
-    def __lt__(self, other):
-
-        t1 = self.suit, self.rank
-        t2 = other.suit, other.rank
-        return t1 < t2
+       t1 = self.suit, self.rank
+       t2 = other.suit, other.rank
+       return t1 < t2
 
 class Deck:
-    """ This represents a deck of Cards """
+   """ This represents a deck of Cards """
 
-    def __init__(self):
-        self.cards = []
-        for suit in range(4):
-            for rank in range(1, 14):
-                card = Card(suit, rank)
-                self.cards.append(card)
+   def __init__(self):
+       self.cards = []
+       for suit in range(4):
+           for rank in range(1, 14):
+               card = Card(suit, rank)
+               self.cards.append(card)
 
-    def __str__(self):
-        res = []
-        for card in self.cards:
-            res.append(str(card))
-        return '\n'.join(res)
+   def __str__(self):
+       res = []
+       for card in self.cards:
+           res.append(str(card))
+       return '\n'.join(res)
 
-    def add_card(self, card):
-        self.cards.append(card)
+   def add_card(self, card):
+       self.cards.append(card)
+   
+   def remove_card(self, card):
+       self.cards.remove(card)
 
-    def remove_card(self, card):
-        self.cards.remove(card)
+   def pop_card(self, i=-1):        
+       return self.cards.pop(i)
 
-    def pop_card(self, i=-1):        
-        return self.cards.pop(i)
+   def shuffle(self):        
+       random.shuffle(self.cards)
 
-    def shuffle(self):        
-        random.shuffle(self.cards)
+   def sort(self):       
+       self.cards.sort()
 
-    def sort(self):       
-        self.cards.sort()
-
-    def move_cards(self, hand, num):
-        for i in range(num):
-            hand.add_card(self.pop_card())
-
+   def move_cards(self, hand, num):
+       for i in range(num):
+           hand.add_card(self.pop_card())
 class Hand(Deck):
 
     """ This represents hand of Player """
