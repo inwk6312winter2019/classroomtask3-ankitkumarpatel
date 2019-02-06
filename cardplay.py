193eab35374c9b372811260504220541bf1a4041
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-05 20:24:19 -0800

    added defferent methods to invoke on card objects

--- cardplay.py
+++ cardplay.py
@@ -1,1 +1,37 @@
 # my code for card play game
+
+
+
+class Card():
+   """ This creates a card object"""
+
+    suit_names = ['Clubs','Diamonds','Hearts','Spades']
+    rank_names = [None,'Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+
+    def __init__(self,suit=1,rank=12):
+        self.suit=suit
+        self.rank=rank
+
+    def __str__(self):
+        return '%s of %s' % (Card.rank_names[self.rank],Card.suit_names[self.suit])
+
+
+    def __eq__(self, other):
+        
+        return self.suit == other.suit and self.rank == other.rank
+
+    def __lt__(self, other):
+        
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 < t2
+
+
+
+
+
+ace_of_spade=Card(1,3)
+print(ace_of_spade)
+
+
+
